c2afd7ff4e4b8eb6009d5d0c66a5f370689c1876
praniil <pparajuli15@gmail.com>
2023-11-06 19:52:18 +0545

    added a easy interview question

---
+++ hello.js
@@ -0,0 +1,14 @@
+// interview question easy
+var string1 = "hello";
+var stringLen = string1.length;
+console.log(stringLen);
+for (var i = 0; i < string1.length; i++) {
+    if (i % 2 == 0) {
+        var ustring = string1[i];
+        ustring = ustring.toUpperCase();
+        console.log(ustring);
+    }
+    else {
+        console.log(string1[i]);
+    }
+}
---
+++ hello.ts
@@ -0,0 +1,16 @@
+// interview question easy
+
+let  string1 : string = "hello"
+let stringLen : number = string1.length;
+console.log(stringLen)
+
+for(let i = 0; i < string1.length ; i++) {
+    if ( i % 2 == 0) {
+        let ustring = string1[i] 
+        ustring = ustring.toUpperCase();
+        console.log(ustring);
+    }
+    else{
+        console.log(string1[i]);
+    }
+}
